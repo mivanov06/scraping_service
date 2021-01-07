140a6fcbde6f2b9dc822a5557786ee6b6cebea6b
mivanov06 <mivanov06@yandex.ru>
2021-01-07 13:54:10 +1000

    Create model Language Migration

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
+
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название населенного пункта')
-    slug = models.CharField(max_length=50, blank=True, verbose_name='Короткое название')
+    name = models.CharField(max_length=50, verbose_name='Название населенного пункта', unique=True)
+    slug = models.CharField(max_length=50, blank=True, verbose_name='Короткое название', unique=True)
 
     class Meta:
         verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Названия населенных пунктов'
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, verbose_name='Короткое название', unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
 
     def __str__(self):
         return self.name
