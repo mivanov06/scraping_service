7598aba49332db597bdacad554b2399a7219d51d
mivanov06 <mivanov06@yandex.ru>
2021-01-07 16:15:13 +1000

    Add home.html (Bootstrap)

--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .view import home
+from scraping.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', home_view),
 ]
 
 #admin.site.site_header = 'Администрирование сайта'
