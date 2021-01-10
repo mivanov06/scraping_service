c68be43cba9dfa02cf404907f0285d6c95cc5bce
mivanov06 <mivanov06@yandex.ru>
2021-01-11 00:49:18 +1000

    Edit views.py Добавление фильтра

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -3,7 +3,15 @@ from .models import Vacancy
 
 
 def home_view(request):
-
-    print(request.POST)
-    qs = Vacancy.objects.all()
+    # print(request.GET)
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    _filter = {}
+    if city or language:
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+    qs = Vacancy.objects.filter(**_filter)
     return render(request, 'scraping/home.html', {'object_list': qs})
