1b020749e010dafbed0f31d8a5602a916859c72f
mivanov06 <mivanov06@yandex.ru>
2021-01-11 20:27:40 +1000

    Add forms.py Добавление формы для ввода значений

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home_view(request):
     # print(request.GET)
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
     qs = []
     _filter = {}
     if city or language:
         if city:
-            _filter['city__name'] = city
+            _filter['city__slug'] = city
         if language:
-            _filter['language__name'] = language
+            _filter['language__slug'] = language
     qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
