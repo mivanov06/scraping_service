48e87b58cff405f336333cadcc0e72657335166f
mivanov06 <mivanov06@yandex.ru>
2021-01-13 23:23:12 +1000

    38-40 Create app Accounts

--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scraping',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +85,9 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+
+AUTH_USER_MODEL = 'accounts.MyUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
