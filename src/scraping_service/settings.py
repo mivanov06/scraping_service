c8bcf5d361d6ace179c472c3393738b9fe92d752
mivanov06 <mivanov06@yandex.ru>
2021-01-13 23:31:36 +1000

    38-40 Create app Accounts

--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraping',
-    'accounts',
+    'scraping.apps.ScrapingConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
